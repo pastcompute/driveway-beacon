2214c8a9965cc9d98cd04b64f9dd445250f4885c
Andrew McDonnell <bugs@andrewmcdonnell.net>
2021-12-28 11:23:48 +1030

    Working 1st gen dump

--- pydump.py
+++ pydump.py
@@ -1,21 +1,47 @@
 import serial
 import re
+import time
+import math
+
 ser = serial.Serial('/dev/ttyUSB1', 115200, parity=serial.PARITY_NONE, stopbits=1, rtscts=0, xonxoff=0, timeout=None)
 ser.flushInput()
 
 pat = re.compile(" ")
 
+rssi_valid = False
+t0 = 0
+t1 = 0
+
 while True:
     try:
         bytes = ser.readline()
+        t1 = time.time()
         #decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         #print(decoded_bytes)
         if len(bytes) < 3:
             # obviously not hex address:
+            #print(bytes)
+            rssi_valid = False
             continue
         aa = bytes[0:3]
         if aa != "00:":
-            # work out how to handle larger packets later
+            s = bytes.decode("utf-8").rstrip().lstrip()
+            if s[0:3] == "Rx ":
+                # Note: rssi_valid should now be true...
+                continue
+            if s[0:6] == "[DBUG]":
+                m = re.search(r"rssi_pkt=(-{0,1}[0-9][0-9]*) snr_pkt=(-{0,1}[0-9][0-9]*)", s)
+                if (m):
+                    #print(m.group(1), m.group(2))
+                    #print("rssi,%s,%s" % (m.group(1), m.group(2)))
+                    rssi = int(m.group(1))
+                    snr = int(m.group(2))
+                    t0 = t1
+                    rssi_valid = True
+                continue
+            # either an error or debug we can filter out, or work out how to handle larger packets later
+            print("UNKNOWN,%s" % bytes.decode("utf-8").rstrip().lstrip())
+            rssi_valid = False
             continue
         bytes2 = pat.split(bytes[4:])
         payload_len = int(bytes2[0], 16)
@@ -37,10 +63,18 @@ while True:
         tempC = int(bytes2[10], 16)
         baseline = int(bytes2[11], 16)
 
-        print counter, ticks10*10, magnitude, tempC, baseline
+        if t0 > 0:
+            t1 = t0  # use earlier time if we parsed an rssi msg before the daat
 
-
-
+        g = time.gmtime(t1)
+        t = time.strftime("%Y-%m-%dT%H:%M:%S.", g)
+        ms = math.modf(t1)[0] * 1000
+        if not rssi_valid:
+            rssi = -1
+            snr = -1
+        print("data,%.03f,%s%03d,%d,%d,%d,%d,%d,%s,%s" % (t1, t, ms, counter, ticks10*10, magnitude, tempC, baseline, rssi, snr))
+        rssi_valid = False
+        t0 = 0
     except KeyboardInterrupt as e:
         break
     except Exception as e2:
